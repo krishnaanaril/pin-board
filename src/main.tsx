15f96ed4e425710dc65735f9165077147b1d743d
krishnaanaril <krishnamohan.a.m@gmail.com>
2024-12-22 14:07:49 +0530

    feat: implement lazy loading for Home, Saved, and Lists components in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+import { lazy, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router'
-import Home from './components/Home.tsx'
-import Saved from './components/Saved.tsx'
-import Lists from './components/Lists.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router';
+
+const Home = lazy(() => import('./components/Home.tsx'));
+const Saved = lazy(() => import('./components/Saved.tsx'));
+const Lists = lazy(() => import('./components/Lists.tsx'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
