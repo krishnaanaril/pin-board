6ba253d34822ba578c09f6a4e39c627d487ec143
krishnaanaril <krishnamohan.a.m@gmail.com>
2024-12-23 10:14:18 +0530

    feat: add About and Settings components with routing

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router';
 
+// Dynamic imports
 const Home = lazy(() => import('./components/Home.tsx'));
 const Saved = lazy(() => import('./components/Saved.tsx'));
 const Lists = lazy(() => import('./components/Lists.tsx'));
+const Settings = lazy(() => import('./components/Settings.tsx'));
+const About = lazy(() => import('./components/About.tsx'));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -16,6 +19,8 @@ createRoot(document.getElementById('root')!).render(
           <Route index element={<Home />} />
           <Route  path="saved" element={<Saved />}/>
           <Route  path="lists" element={<Lists />}/>
+          <Route  path="settings" element={<Settings />}/>
+          <Route  path="about" element={<About />}/>
         </Route>
       </Routes>
     </BrowserRouter>
