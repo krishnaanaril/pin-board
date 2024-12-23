fcff39d2ee82b8911a82d5985a40cefdd1d3d4dc
krishnaanaril <krishnamohan.a.m@gmail.com>
2024-12-23 20:55:46 +0530

    feat: enhance Settings component with Card layout for data import/export functionality

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -3,9 +3,10 @@ import PageHeader from "./PageHeader";
 import { Button } from "./ui/button";
 import { SaveData } from "@/lib/helpers";
 import { Input } from "./ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 
 function Settings() {
-    
+
     const { savedLists, savedLocations, appendSavedLists, appendSavedLocations } = usePinBoardStore();
 
     function exportData() {
@@ -37,15 +38,23 @@ function Settings() {
     return (
         <div className="block h-full w-screen overflow-y-scroll">
             <PageHeader headerText="Settings" />
-            <div className="p-4">
-                <h2 className="text-2xl font-bold underline underline-offset-1">Export Data</h2>
-                <Button onClick={exportData} >Export</Button>
-            </div>
-            <div className="p-4">
-                <h2 className="text-2xl font-bold underline underline-offset-1">Import Data</h2>
-                <Input id="file-input" type="file" placeholder="Upload json file"/>
-                <Button onClick={importData} >Import</Button>
-            </div>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Export Data</CardTitle>                    
+                </CardHeader>
+                <CardContent>
+                    <Button onClick={exportData} >Export</Button>
+                </CardContent>
+            </Card>
+            <Card>
+                <CardHeader>
+                    <CardTitle>Import Data</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <Input className="my-4" id="file-input" type="file" placeholder="Upload json file" />
+                    <Button onClick={importData} >Import</Button>
+                </CardContent>
+            </Card>
         </div>
     )
 }
