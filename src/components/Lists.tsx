424920b40ef0cd52f8cc5f9e67d1d12908657a1f
krishnaanaril <krishnamohan.a.m@gmail.com>
2024-12-24 09:46:50 +0530

    refactor: remove unused imports and commented code in Lists and Saved components

--- src/components/Lists.tsx
+++ src/components/Lists.tsx
@@ -1,11 +1,8 @@
 import { ListDetailsWithPlaces } from "@/store/model";
 import usePinBoardStore from "@/store/pinboard-store";
-// import { Button } from "./ui/button";
-// import { ArrowLeft } from "lucide-react";
 import SaveList from "./SaveList";
 import ListCard from "./ListCard";
 import PageHeader from "./PageHeader";
-// import PageFooter from "./PageFooter";
 
 function Lists() {
 
@@ -14,34 +11,20 @@ function Lists() {
   const listsWithPlaces: ListDetailsWithPlaces[] = savedLists.map(list => ({ ...list, places: savedLocations.filter(location => location.listId === list.id) }));
   const lists = listsWithPlaces.map(list => <ListCard key={list.id} list={list} />);
 
-  // const handleBackClick = () => {
-  //   window.history.back();
-  // };
-
   return (
     <div className="block h-full w-full">
       <PageHeader headerText="Lists">
         <SaveList />
       </PageHeader>
-      <div>
+      <div className="h-3/4 flex flex-col justify-center">
         {lists}
         {lists.length === 0 && (
           <div className="flex flex-col justify-center items-center p-4">
-            <img className="size-48" src="/public/nodata.svg" alt="No data" />
-            <p className="text-center text-xl font-bold my-2">No saved lists found</p>
+            <img className="size-48" src="/nodata.svg" alt="No data" />
+            <p className="text-center font-bold my-2">No saved lists found</p>
           </div>
         )}
       </div>
-      {/* <PageFooter />
-      <div className="fixed w-full bottom-0 py-4 flex place-content-evenly bg-opacity-50 backdrop-blur-lg">
-        <Button id="back-button" onClick={handleBackClick} asChild>
-          <div>
-            <ArrowLeft />
-            Back
-          </div>
-        </Button>
-        <SaveList />
-      </div> */}
     </div>
   );
 }
--- src/components/Saved.tsx
+++ src/components/Saved.tsx
@@ -1,11 +1,8 @@
-// import { ArrowLeft } from "lucide-react";
-// import { Button } from "./ui/button";
 import { useLocation, useSearchParams } from "react-router";
 import usePinBoardStore from "@/store/pinboard-store";
 import LocationCard from "./LocationCard";
 import { useEffect, useState } from "react";
 import PageHeader from "./PageHeader";
-// import PageFooter from "./PageFooter";
 
 function Saved() {
 
@@ -24,31 +21,18 @@ function Saved() {
         setLocations(filteredLocations.map(location => <LocationCard key={location.id} location={location} />));
     }, [location, searchParams, savedLocations]);
 
-    // function handleBackClick() {
-    //     window.history.back();
-    // }
-
     return (
         <div className="h-full">
             <PageHeader headerText="Saved Places"/>
-            <div>
+            <div className="h-3/4 flex flex-col justify-center">
                 {locations}
                 {locations.length === 0 && (
                     <div className="flex flex-col justify-center items-center p-4">
-                        <img className="size-48" src="/public/nodata.svg" alt="No data" />
-                        <p className="text-center text-xl font-bold my-2">No saved locations found</p>
+                        <img className="size-48" src="/nodata.svg" alt="No data" />
+                        <p className="text-center font-bold my-2">No saved locations found</p>
                     </div>
                 )}
             </div>
-            {/* <PageFooter/>
-            <div className="fixed w-full bottom-0 py-4 flex place-content-evenly bg-opacity-50 backdrop-blur-lg">
-                <Button id="back-button" onClick={handleBackClick} asChild>
-                    <div>
-                        <ArrowLeft />
-                        Back
-                    </div>
-                </Button>
-            </div> */}
         </div>
     )
 }
