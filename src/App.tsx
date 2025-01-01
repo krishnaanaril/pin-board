d463390e552119cee1f26d229ac0db7ab93d4e64
krishnaanaril <krishnamohan.a.m@gmail.com>
2025-01-01 13:30:40 +0530

    feat: implement theme toggle functionality with dark mode support and update relevant components

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { Button } from './components/ui/button'
 import SaveLocation from './components/SaveLocation'
 import { SearchForm } from './components/SearchForm'
 import usePinBoardStore from './store/pinboard-store'
+import { ThemeProvider } from './components/ThemeProvider'
 
 function App() {
 
@@ -18,43 +19,45 @@ function App() {
   }
 
   return (
-    <div className='h-dvh flex flex-col md:max-w-screen-md md:mx-auto'>
-      <div className='h-full overflow-y-auto'>
-        <Outlet />
-        <Toaster />
-      </div>
-      {location.pathname === '/' && (
-        <div className='w-screen md:max-w-screen-md md:mx-auto'>
-          <div className="relative right-2 flex flex-col md:right-4">
-            <div className="my-2 flex flex-row justify-end">
-              <Button id="current-location-button" onClick={goToCurrentLocation}>
-                <LocateFixed size={24} />
-              </Button>
-            </div>
-            <div className='my-2 flex flex-row justify-end'>
-              <SaveLocation />
-            </div>
-            <div className='my-2 ml-2 flex flex-row justify-end'>
-              <SearchForm />
-            </div>
-          </div>          
+    <ThemeProvider defaultTheme="light" storageKey="pin-board-theme">
+      <div className='h-dvh flex flex-col md:max-w-screen-md md:mx-auto'>
+        <div className='h-full overflow-y-auto'>
+          <Outlet />
+          <Toaster />
         </div>
-      )}
+        {location.pathname === '/' && (
+          <div className='w-screen md:max-w-screen-md md:mx-auto'>
+            <div className="relative right-2 flex flex-col md:right-4">
+              <div className="my-2 flex flex-row justify-end">
+                <Button id="current-location-button" onClick={goToCurrentLocation}>
+                  <LocateFixed size={24} />
+                </Button>
+              </div>
+              <div className='my-2 flex flex-row justify-end'>
+                <SaveLocation />
+              </div>
+              <div className='my-2 ml-2 flex flex-row justify-end'>
+                <SearchForm />
+              </div>
+            </div>
+          </div>
+        )}
 
-      <div className='bg-opacity-50 py-2 backdrop-blur-lg flex flex-row justify-between max-w-screen-md md:mx-auto w-full md:w-screen-sm md:rounded-full md:mb-2'>
-        <Link className='w-1/3 flex flex-col items-center' id="saved-link" to="/saved">
-          <Bookmark />
-          Saved
-        </Link>
-        <Link className='w-1/3 flex flex-col items-center' id="search-link" to="/">
-          <Search />
-          Search
-        </Link>
-        <div className='w-1/3 flex flex-col items-center'>
-          <SideMenu />
+        <div className='bg-opacity-50 py-2 backdrop-blur-lg flex flex-row justify-between max-w-screen-md md:mx-auto w-full md:w-screen-sm md:rounded-full md:mb-2'>
+          <Link className='w-1/3 flex flex-col items-center' id="saved-link" to="/saved">
+            <Bookmark />
+            Saved
+          </Link>
+          <Link className='w-1/3 flex flex-col items-center' id="search-link" to="/">
+            <Search />
+            Search
+          </Link>
+          <div className='w-1/3 flex flex-col items-center'>
+            <SideMenu />
+          </div>
         </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 
--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,6 +1,6 @@
 function PageHeader({ headerText, children }: { headerText: string, children?: React.ReactNode }) {
     return (
-        <header className="h-32 flex flex-col-reverse pl-4 pb-4 bg-yellow-300">
+        <header className="h-32 flex flex-col-reverse pl-4 pb-4 bg-yellow-300 dark:bg-neutral-600 dark:text-yellow-300">
             <div className="flex flex-row justify-between">
                 <h1 className="text-4xl font-bold">{headerText}</h1>
                 <div className="pr-4">
